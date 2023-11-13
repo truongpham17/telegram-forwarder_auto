3a3abafff7630df53a892d1b7e8c169a46df636b
Truong Pham <truong.pham@zoi.tech>
2023-11-13 16:52:24 +0700

    edit the library

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@
 from telethon import TelegramClient, events
 from decouple import config
 import logging
-from telethon.sessions import StringSession
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
@@ -28,25 +27,24 @@ FROM_ = config("FROM_CHANNEL")
 TO_ = config("TO_CHANNEL")
 
 FROM = [int(i) for i in FROM_.split()]
-TO = [int(i) for i in TO_.split()]
+# TO = [int(i) for i in TO_.split()]
 
 try:
-    BotzHubUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
-    BotzHubUser.start()
+    client = TelegramClient(SESSION, APP_ID, API_HASH)
+    client.start()
 except Exception as ap:
     print(f"ERROR - {ap}")
     exit(1)
 
-@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
+
+
+@client.on(events.NewMessage(incoming=False, chats=FROM))
 async def sender_bH(event):
-    for i in TO:
-        try:
-            await BotzHubUser.send_message(
-                i,
-                event.message
-            )
-        except Exception as e:
-            print(e)
+    print("receive message")
+    try:
+        await client.send_message('@trading_signal_bot', event.message)
+    except Exception as e:
+        print(e)
 
 print("Bot has started.")
-BotzHubUser.run_until_disconnected()
+client.run_until_disconnected()

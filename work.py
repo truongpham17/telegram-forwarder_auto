94090ef840cebbc521cea6db737ba96ae879952e
Truong Pham <truong.pham@zoi.tech>
2025-05-02 20:03:34 +0700

    fiox

--- work.py
+++ work.py
@@ -2,7 +2,6 @@ from telethon import TelegramClient, events
 from decouple import config
 import requests
 
-
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 SESSION = config("SESSION")
@@ -12,6 +11,7 @@ TO_ = config("TO_CHANNEL")
 client = TelegramClient(SESSION, APP_ID, API_HASH)
 NODE_ENDPOINT = 'http://localhost:3000/trade-signal'
 
+@client.on(events.NewMessage(incoming=True, chats=FROM_))
 async def handler(event):
     message = event.message.message
     print("Forwarding message:", message)

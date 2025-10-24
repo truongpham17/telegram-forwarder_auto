8741ef6c3ecfbcee23a38d2caa65bd13a236610b
Truong Pham <truongphamminh17@gmail.com>
2025-10-24 12:43:04 +0000

    fix

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ print(f"Title: {channel.title}")
 print(f"ID: {channel.id}")
 
 
-@client.on(events.NewMessage())
+@client.on(events.NewMessage(chats=FROM_))
 async def sender_bH(event):
     try:
         # await client.send_message('@trading_signal_bot', event.message)
